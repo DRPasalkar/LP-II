e074134388a282c0afbff8ddd264ccb5b398f162
DRPasalkar <darsh.stud2025@gmail.com>
2024-05-09 22:17:33 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,5 +1,5 @@
 print("This is the second project of cloud computing.\n Where we configured the google app engine")
 def call():
-  print("We made a function call")
+  print("We made a function call\n need to do some changes")
 
 call()
